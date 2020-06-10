cbf2e032cd5491200405aece34ec23b4e4003f46
TLBN <tete.leticia@gmail.com>
2020-06-10 11:22:08 -0300

    add round metod; add starter operations; add loop metod;

--- BanckUp/src/entities/Account.java
+++ BanckUp/src/entities/Account.java
@@ -1,26 +1,25 @@
 package entities;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Locale;
 import java.util.Scanner;
 
 
-
 public class Account {
 
-	private double balance;
 	private static int number;
+	char ans;
+	private double balance;
 	private String holder;
-	
 	Scanner sc;
-	
+
 	public Account () {
-		
+
 		this.sc = new Scanner(System.in);
 		this.sc.useLocale(Locale.US);
-		
 		this.balance = 0;
 
-		
 		System.out.println("Enter account number: ");
 		Account.number = sc.nextInt();
 		System.out.println("Enter account Holder: ");
@@ -29,69 +28,82 @@ public class Account {
 		System.out.println("New Account: Holder -" + holder);
 		System.out.println("Number - " + Account.number);
 
-		char ans = 'y';
-		
-		while (ans == 'y') {
-			
-			System.out.println("Enter operation: Deposit (d) Withdraw (w) ");
-			char tipe = this.sc.next().charAt(0);
-		
-			this.operations(tipe);
-			
-			System.out.println("Next operation ? y/n ");
-			ans = this.sc.next().charAt(0);
-			
-		}
-		
-		System.out.println( "Account: " + getNumber() 
-		+ ", " + "Holder :" + getHolder() 
-		+ ", Balance: $ " + getBalance());
-		
+		System.out.println("Wish to make an operation? y/n");
+		this.ans = sc.next().charAt(0);
+
+
+		this.loopOperation(ans);
+
+		System.out.println( "Account: " + getNumber()
+				+ ", " + "Holder :" + getHolder()
+				+ ", Balance: $ " + round(getBalance(),2));
+
 		sc.close();
 
+		System.out.println("---------------END OF SESSION-----------");
 	}
-	
-	public void operations(char tipe) {
-		
-		if (tipe == 'd') {
-			System.out.println("Enter deposit value: ");
-			double value = sc.nextDouble();
-			this.deposit(value);
 
-		} else if (tipe == 'w') {
-			System.out.println("Enter Withdraw value: ");
-			double value = sc.nextDouble();
-			this.withdraw(value);
+	public void loopOperation(char ans) {
+		while (ans == 'y') {
+
+			System.out.println("Enter operation: Deposit (d) Withdraw (w) ");
+			char tipe = this.sc.next().charAt(0);
+
+			this.operations(tipe);
+
+			System.out.println("Next operation ? y/n ");
+			ans = this.sc.next().charAt(0);
 		}
-		
-		System.out.println( "Account: " + getNumber() 
-		+ ", " + "Holder :" + getHolder() 
-		+ ", Balance: $ " + getBalance());
-		
+	}
+		public void operations(char opTipe) {
+
+			if (opTipe == 'd') {
+				System.out.println("Enter deposit value: ");
+				double value = sc.nextDouble();
+				this.deposit(value);
+
+			} else if (opTipe == 'w') {
+				System.out.println("Enter Withdraw value: ");
+				double value = sc.nextDouble();
+				this.withdraw(value);
+			}
+
+			System.out.println("Account: " + getNumber()
+					+ ", " + "Holder :" + getHolder()
+					+ ", Balance: $ " + round(getBalance(),2));
+		}
+
+		// METODO DE ARREDONDAMENTO GENÉRICO DE DOUBLE
+	private static double round(double value, int places) {
+		if (places < 0) throw new IllegalArgumentException();
+
+		BigDecimal bd = new BigDecimal(Double.toString(value));
+		bd = bd.setScale(places, RoundingMode.HALF_UP);
+		return bd.doubleValue();
 	}
 
-	public void deposit(double amount) {
-		this.balance += amount;
-	}
+		public void deposit(double amount) {
+			this.balance += amount;
+		}
 
-	public void withdraw(double amount) {
-		this.balance -= amount + 5.00;
-	}
+		public void withdraw(double amount) {
+			this.balance -= amount + 5.00;
+		}
 
-	public double getBalance() {
-		return this.balance;
-	}
+		public double getBalance() {
+			return this.balance;
+		}
 
-	public int getNumber() {
-		return Account.number;
-	}
+		public int getNumber() {
+			return Account.number;
+		}
 
-	public void setHolder(String holder) {
-		this.holder = holder;
-	}
+		public void setHolder(String holder) {
+			this.holder = holder;
+		}
 
-	public String getHolder() {
-		return this.holder;
-	}
+		public String getHolder() {
+			return this.holder;
+		}
 
-}
+	}
